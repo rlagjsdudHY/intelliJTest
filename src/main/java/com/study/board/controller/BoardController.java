2843d42a3ae513c17ef970ada109ec8e78e13c7e
Computer <9550147s@gmail.com>
2023-05-28 20:21:30 +0900

    third commit

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -22,12 +23,13 @@ public class BoardController {
   }
 
   @PostMapping("/board/writepro")
-  public String boardWritePro(Board board) {
+  public String boardWritePro(Board board, Model model) {
 
     boardService.write(board);
 
-
-    return "";
+    model.addAttribute("message","글작성이 완료되었습니다");
+    model.addAttribute("searchUrl", "/board/list");
+    return "message";
   }
 
   @GetMapping("/board/list")
@@ -44,6 +46,36 @@ public class BoardController {
     model.addAttribute("board", boardService.boardView(id));
     return "boardview";
     }
+
+    @GetMapping("/board/delete")
+  public String boardDelete(Integer id ){
+
+      boardService.boardDelete(id);
+
+      return "redirect:/board/list";
+    }
+
+      @GetMapping("/board/modify/{id}")
+  public String boardModify(@PathVariable("id") Integer id,
+                            Model model){
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardmodify";
+      }
+
+      @PostMapping("/board/update/{id}")
+  public String boardUpdate(@PathVariable("id") Integer id, Board board){
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/board/list";
+      }
+
 
 }
 
